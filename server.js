ee51298a6a6ad3056e16ba1e1b40e0b34b9d237d
Mdhamed <mdhamed0238@gmail.com>
2022-08-03 19:36:57 +0000

    Simple changes

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/api/goals', require('./server/routes/goalRoutes'));
-app.use('/api/movies', require('./server/routes/movieRoutes'));
 app.use('/api/users', require('./server/routes/userRoutes'));
 
 
